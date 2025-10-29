046b3b3c20ade51f3d200f404fa82ff8b521e0e9
squaveboyz <167702571+squaveboyz@users.noreply.github.com>
2025-10-29 14:56:22 +0100

    Create Storico.tsx

---
+++ src/pages/Storico.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Storico() {
+  return (
+    <div>
+      <h1>Storico</h1>
+      <p>Visualizza lo storico delle coltivazioni, rese e trattamenti.</p>
+    </div>
+  );
+}
