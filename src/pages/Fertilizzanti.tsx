3b8e1fed232055f0fc035cca4769d4188812b3ca
squaveboyz <167702571+squaveboyz@users.noreply.github.com>
2025-10-29 14:53:16 +0100

    Create Fertilizzanti.tsx

---
+++ src/pages/Fertilizzanti.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Fertilizzanti() {
+  return (
+    <div>
+      <h1>Fertilizzanti</h1>
+      <p>Gestisci i fertilizzanti per le tue colture.</p>
+    </div>
+  );
+}
