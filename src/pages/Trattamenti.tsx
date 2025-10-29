b1424f78c06c1c587a7142ccc8f66164603e5aed
squaveboyz <167702571+squaveboyz@users.noreply.github.com>
2025-10-29 14:54:20 +0100

    Create Trattamenti.tsx

---
+++ src/pages/Trattamenti.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Trattamenti() {
+  return (
+    <div>
+      <h1>Trattamenti</h1>
+      <p>Gestisci i trattamenti per le tue colture.</p>
+    </div>
+  );
+}
