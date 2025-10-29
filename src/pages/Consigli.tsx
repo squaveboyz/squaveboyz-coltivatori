dd8c2084b58966ff3c4497066a2c7e95154d071c
squaveboyz <167702571+squaveboyz@users.noreply.github.com>
2025-10-29 14:55:39 +0100

    Create Consigli.tsx

---
+++ src/pages/Consigli.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Consigli() {
+  return (
+    <div>
+      <h1>Consigli</h1>
+      <p>Ricevi consigli personalizzati per la coltivazione.</p>
+    </div>
+  );
+}
